53e489a4494a862346c906721997ce271a69447e
Idera-oluwa <ideraoluwaisa2003@gmail.com>
2024-06-07 07:10:27 -0700

    new commit

---
+++ src/app/[locale]/(pages)/donations/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '@/components/donations/header'
+
+const page = () => {
+  return (
+    <div className='pt-[3rem] md:pt-[7rem] mb-[3rem] md:mb-0'>
+        <Header />
+    </div>
+  )
+}
+
+export default page
---
+++ src/components/donations/header.js
@@ -0,0 +1,72 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="w-[90vw] mx-auto mt-[2rem] lg:mt-[6rem]">
+      <div className="w-[90%] mx-auto mt-[0rem] lg:mt-[5rem] flex flex-row justify-between items-center lg:items-start">
+        <div className="flex flex-row mt-[0.5rem] lg:gap-[1rem] items-center lg:items-start">
+          <p className="font-normal text-[20px] leading-6 lg:text-[40px] lg:leading-[58px] text-white">
+            Donations
+          </p>
+        </div>
+        <div className="flex flex-row gap-[0.5rem]">
+          <a
+            href="https://www.facebook.com/Lifecomfm.co.uk/"
+            target="_blank"
+            rel="noreferrer">
+            {" "}
+            <img
+              src="/images/home/facebook.svg"
+              alt=""
+              className="cursor-pointer lg:w-[30px] lg:h-[30px] w-[17px] h-[17px]"
+            />{" "}
+          </a>
+          <a
+            href="https://x.com/LifecomRadio"
+            target="_blank"
+            rel="noreferrer"
+          ><img
+            src="/images/home/twitter.svg"
+            alt=""
+            className="cursor-pointer  lg:w-[30px] lg:h-[30px] w-[17px] h-[17px]"
+          /></a>
+          <a
+            href="https://www.instagram.com/lifecom_radioandtelevision/"
+            target="_blank"
+            rel="noreferrer">
+            <img
+              src="/images/home/instagram.svg"
+              alt=""
+              className="cursor-pointer  lg:w-[30px] lg:h-[30px] w-[17px] h-[17px]"
+            />
+          </a>
+          <img
+            src="/images/home/linkedin.svg"
+            alt=""
+            className="cursor-pointer  lg:w-[30px] lg:h-[30px] w-[17px] h-[17px]"
+          />
+        </div>
+      </div>
+      <div className="w-full bg-[#1B2A41] h-[2px] md:h-[5px] mt-4 lg:mt-[3rem] rounded-[10px]"></div>
+      <div className="w-[90%] mx-auto mt-[3rem]">
+        <p className='font-semibold text-[18px] text-white'>You can make contributions through any of the following methods</p>
+        <p className='text-[#CCC9DC] text-[16px] font-normal mt-[0.5rem]'>Do you like what you hear on lifecom radio and would like to contribute in form of donations? </p>
+         <div className='flex flex-col mx-auto w-auto justify-center items-center mt-[8rem]'>
+         <img src='/images/donations/paypal-icon.png' alt='' className='w-[162px] h-[41.4px]'/>
+         <div className='w-[336px] h-[69px] bg-gradient-to-r from-[#0F2E5F80] to-[#259480] mt-[1.5rem] pt-[2px]'>
+          <div className='w-[calc(100%-4px)] bg-[#0C1821] h-[calc(100%-2px)] mx-auto flex justify-center items-center'>
+            <p className='text-[16px] text-white font-medium'>info@lifecomradio.org.uk</p>
+          </div>
+         </div>
+         </div>
+         <div className='flex flex-row justify-between items-center'>
+           <div className='flex flex-col'>
+
+           </div>
+         </div>
+        </div>
+      </div>
+  )
+}
+
+export default Header
