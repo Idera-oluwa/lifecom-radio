050d1784214e716dc52a83dad029e04f0f9e91f4
Idera-oluwa <ideraoluwaisa2003@gmail.com>
2024-06-07 07:32:44 -0700

    new commit

--- src/app/[locale]/(pages)/donations/page.js
+++ src/app/[locale]/(pages)/donations/page.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '@/components/donations/header'
+import Faq from '@/components/getintouch/faq'
 
 const page = () => {
   return (
     <div className='pt-[3rem] md:pt-[7rem] mb-[3rem] md:mb-0'>
         <Header />
+        <Faq />
     </div>
   )
 }
