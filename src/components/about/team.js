c9da412873ab132c581b41c5a65dfb51610f0802
Idera-oluwa <ideraoluwaisa2003@gmail.com>
2024-06-08 10:41:36 -0700

    team template done

--- src/components/about/team.js
+++ src/components/about/team.js
@@ -1,21 +1,138 @@
 import React from 'react'
-import Link from "next/link";
 import initTranslations from '@/i18n';
 
 const team = async ({locale}) => {
   const { t } = await initTranslations(locale,['about']);
   const { t:h } = await initTranslations(locale,['home']);
   return (
-    <div className='w-[80vw] mx-auto flex flex-col md:flex-row justify-between mt-[5rem] md:mt-[15rem] mb-[-3rem]'>
-     <div className='flex flex-col items-center md:items-start'>
-        <div className='flex flex-row gap-[0.1rem] md:gap-[1rem] items-end'>
-          <p className='text-white text-[12px] md:text-[28px] font-semibold'>{t('Meet the Team')}</p>
-          <img src='/images/about/golden-mic.png' alt='' className='md:w-[61px] md:h-[61px] w-[28px] h-[28px]'/>
-        </div>
-        <p className='w-[90%] text-center md:text-left md:w-[297px] font-normal text-white text-[10px] md:text-[16px] leading-[12.1px] md:leading-[22.3px] mt-[0.5rem] md:mt-[1.2rem]'>{t('Get to know the individuals dedicated to making LifeCom radio an uplifting and entertaining radio station.')}</p>
-        <Link href="/about"><button className='bg-[#1B2A41] w-[116px] h-[37px] rounded-[4px] hidden md:flex justify-center items-center font-medium text-[14px] text-white mt-[2.5rem]'>{h('Learn More')}</button></Link>
+    <div className='w-[80vw] mx-auto flex flex-col justify-between mt-[5rem] md:mt-[15rem] mb-[-3rem]'>
+     <div className='flex flex-col items-center'>
+          <p className='text-white text-[12px] md:text-[40px] font-medium'>{t('Our Team')}</p>
+        <p className='w-[90%] text-center font-normal text-white text-[10px] md:text-[16px] leading-[12.1px] md:leading-[22.3px] mt-[0.5rem] md:mt-[1.2rem]'>{t('Get to know the individuals dedicated to making LifeCom radio an uplifting and entertaining radio station.')}</p>
      </div>
-     <div className='flex flex-row gap-[1rem] mt-[2rem] md:mt-0'>
+     <div className='mt-[2.5rem] grid grid-cols-fluid w-[70vw] mx-auto'>
+     <div className='bg-gradient-to-b from-[#152947] to-[#456CA7] w-[213px] h-[289px] rounded-[4px] pt-[2px] mt-[1.5rem]'>
+     <div className='w-[calc(100%-4px)] bg-[#0C1821] h-[calc(100%-2px)] mx-auto p-[1rem] flex flex-col justify-center items-center rounded-[4px]'>
+      <div>
+      <img src='/images/about/team1.png' className='w-[165px] h-[160px] mx-auto'/>
+     <p className='text-white text-[14px] font-semibold text-left mt-[0.5rem]'>Andrew Lawrence</p>
+     <p className='text-[#CCC9DC] text-[14px] font-semibold'>O.A.P</p> 
+     <div className='flex flex-row items-center mt-[0.5rem] gap-[0.5rem]'>
+       <img src='/images/about/Facebook.png' alt='' className='w-[7.8px] h-[14.58px]'/>
+       <img src='/images/about/linkedin.png' alt='' className='w-[14.08px] h-[14.08px]'/>
+       <img src='/images/about/twitter.png' alt='' className='w-[14px] h-[14px]'/>
+     </div>
+      </div>
+      
+     </div>
+     </div>
+     <div className='bg-gradient-to-b from-[#152947] to-[#456CA7] w-[213px] h-[289px] rounded-[4px] pt-[2px] mt-[1.5rem]'>
+     <div className='w-[calc(100%-4px)] bg-[#0C1821] h-[calc(100%-2px)] mx-auto p-[1rem] flex flex-col justify-center items-center rounded-[4px]'>
+      <div>
+      <img src='/images/about/team1.png' className='w-[165px] h-[160px] mx-auto'/>
+     <p className='text-white text-[14px] font-semibold text-left mt-[0.5rem]'>Andrew Lawrence</p>
+     <p className='text-[#CCC9DC] text-[14px] font-semibold'>O.A.P</p> 
+     <div className='flex flex-row items-center mt-[0.5rem] gap-[0.5rem]'>
+       <img src='/images/about/Facebook.png' alt='' className='w-[7.8px] h-[14.58px]'/>
+       <img src='/images/about/linkedin.png' alt='' className='w-[14.08px] h-[14.08px]'/>
+       <img src='/images/about/twitter.png' alt='' className='w-[14px] h-[14px]'/>
+     </div>
+      </div>
+      
+     </div>
+     </div>
+     <div className='bg-gradient-to-b from-[#152947] to-[#456CA7] w-[213px] h-[289px] rounded-[4px] pt-[2px] mt-[1.5rem]'>
+     <div className='w-[calc(100%-4px)] bg-[#0C1821] h-[calc(100%-2px)] mx-auto p-[1rem] flex flex-col justify-center items-center rounded-[4px]'>
+      <div>
+      <img src='/images/about/team1.png' className='w-[165px] h-[160px] mx-auto'/>
+     <p className='text-white text-[14px] font-semibold text-left mt-[0.5rem]'>Andrew Lawrence</p>
+     <p className='text-[#CCC9DC] text-[14px] font-semibold'>O.A.P</p> 
+     <div className='flex flex-row items-center mt-[0.5rem] gap-[0.5rem]'>
+       <img src='/images/about/Facebook.png' alt='' className='w-[7.8px] h-[14.58px]'/>
+       <img src='/images/about/linkedin.png' alt='' className='w-[14.08px] h-[14.08px]'/>
+       <img src='/images/about/twitter.png' alt='' className='w-[14px] h-[14px]'/>
+     </div>
+      </div>
+      
+     </div>
+     </div>
+     <div className='bg-gradient-to-b from-[#152947] to-[#456CA7] w-[213px] h-[289px] rounded-[4px] pt-[2px] mt-[1.5rem]'>
+     <div className='w-[calc(100%-4px)] bg-[#0C1821] h-[calc(100%-2px)] mx-auto p-[1rem] flex flex-col justify-center items-center rounded-[4px]'>
+      <div>
+      <img src='/images/about/team1.png' className='w-[165px] h-[160px] mx-auto'/>
+     <p className='text-white text-[14px] font-semibold text-left mt-[0.5rem]'>Andrew Lawrence</p>
+     <p className='text-[#CCC9DC] text-[14px] font-semibold'>O.A.P</p> 
+     <div className='flex flex-row items-center mt-[0.5rem] gap-[0.5rem]'>
+       <img src='/images/about/Facebook.png' alt='' className='w-[7.8px] h-[14.58px]'/>
+       <img src='/images/about/linkedin.png' alt='' className='w-[14.08px] h-[14.08px]'/>
+       <img src='/images/about/twitter.png' alt='' className='w-[14px] h-[14px]'/>
+     </div>
+      </div>
+      
+     </div>
+     </div>
+     <div className='bg-gradient-to-b from-[#36AE99] to-[#18718D70] w-[213px] h-[289px] rounded-[4px] pt-[2px] mt-[1.5rem]'>
+     <div className='w-[calc(100%-4px)] bg-[#0C1821] h-[calc(100%-2px)] mx-auto p-[1rem] flex flex-col justify-center items-center rounded-[4px]'>
+      <div>
+      <img src='/images/about/team1.png' className='w-[165px] h-[160px] mx-auto'/>
+     <p className='text-white text-[14px] font-semibold text-left mt-[0.5rem]'>Andrew Lawrence</p>
+     <p className='text-[#CCC9DC] text-[14px] font-semibold'>O.A.P</p> 
+     <div className='flex flex-row items-center mt-[0.5rem] gap-[0.5rem]'>
+       <img src='/images/about/Facebook.png' alt='' className='w-[7.8px] h-[14.58px]'/>
+       <img src='/images/about/linkedin.png' alt='' className='w-[14.08px] h-[14.08px]'/>
+       <img src='/images/about/twitter.png' alt='' className='w-[14px] h-[14px]'/>
+     </div>
+      </div>
+      
+     </div>
+     </div>
+     <div className='bg-gradient-to-b from-[#36AE99] to-[#18718D70] w-[213px] h-[289px] rounded-[4px] pt-[2px] mt-[1.5rem]'>
+     <div className='w-[calc(100%-4px)] bg-[#0C1821] h-[calc(100%-2px)] mx-auto p-[1rem] flex flex-col justify-center items-center rounded-[4px]'>
+      <div>
+      <img src='/images/about/team1.png' className='w-[165px] h-[160px] mx-auto'/>
+     <p className='text-white text-[14px] font-semibold text-left mt-[0.5rem]'>Andrew Lawrence</p>
+     <p className='text-[#CCC9DC] text-[14px] font-semibold'>O.A.P</p> 
+     <div className='flex flex-row items-center mt-[0.5rem] gap-[0.5rem]'>
+       <img src='/images/about/Facebook.png' alt='' className='w-[7.8px] h-[14.58px]'/>
+       <img src='/images/about/linkedin.png' alt='' className='w-[14.08px] h-[14.08px]'/>
+       <img src='/images/about/twitter.png' alt='' className='w-[14px] h-[14px]'/>
+     </div>
+      </div>
+      
+     </div>
+     </div>
+     <div className='bg-gradient-to-b from-[#36AE99] to-[#18718D70] w-[213px] h-[289px] rounded-[4px] pt-[2px] mt-[1.5rem]'>
+     <div className='w-[calc(100%-4px)] bg-[#0C1821] h-[calc(100%-2px)] mx-auto p-[1rem] flex flex-col justify-center items-center rounded-[4px]'>
+      <div>
+      <img src='/images/about/team1.png' className='w-[165px] h-[160px] mx-auto'/>
+     <p className='text-white text-[14px] font-semibold text-left mt-[0.5rem]'>Andrew Lawrence</p>
+     <p className='text-[#CCC9DC] text-[14px] font-semibold'>O.A.P</p> 
+     <div className='flex flex-row items-center mt-[0.5rem] gap-[0.5rem]'>
+       <img src='/images/about/Facebook.png' alt='' className='w-[7.8px] h-[14.58px]'/>
+       <img src='/images/about/linkedin.png' alt='' className='w-[14.08px] h-[14.08px]'/>
+       <img src='/images/about/twitter.png' alt='' className='w-[14px] h-[14px]'/>
+     </div>
+      </div>
+      
+     </div>
+     </div>
+     <div className='bg-gradient-to-b from-[#36AE99] to-[#18718D70] w-[213px] h-[289px] rounded-[4px] pt-[2px] mt-[1.5rem]'>
+     <div className='w-[calc(100%-4px)] bg-[#0C1821] h-[calc(100%-2px)] mx-auto p-[1rem] flex flex-col justify-center items-center rounded-[4px]'>
+      <div>
+      <img src='/images/about/team1.png' className='w-[165px] h-[160px] mx-auto'/>
+     <p className='text-white text-[14px] font-semibold text-left mt-[0.5rem]'>Andrew Lawrence</p>
+     <p className='text-[#CCC9DC] text-[14px] font-semibold'>O.A.P</p> 
+     <div className='flex flex-row items-center mt-[0.5rem] gap-[0.5rem]'>
+       <img src='/images/about/Facebook.png' alt='' className='w-[7.8px] h-[14.58px]'/>
+       <img src='/images/about/linkedin.png' alt='' className='w-[14.08px] h-[14.08px]'/>
+       <img src='/images/about/twitter.png' alt='' className='w-[14px] h-[14px]'/>
+     </div>
+      </div>
+      
+     </div>
+     </div>
+     </div>
+     {/* <div className='flex flex-row gap-[1rem] mt-[2rem] md:mt-0'>
       <div className='w-[153px] md:bg-[#292E35] h-[208px] rounded-[4px]'>
         <img src='/images/about/team1.png' alt=''/>
        <div className='py-[0.5rem] flex flex-col items-center'>
@@ -44,7 +161,7 @@ const team = async ({locale}) => {
         <p className='text-white text-[10px] md:text-[12px] font-normal'>{t('Engineer')}</p>
        </div>
       </div>
-     </div>
+     </div> */}
     </div>
   )
 }
