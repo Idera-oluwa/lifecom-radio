016e6583b01a18171156d70c7f4459e68da6c9f6
Idera-oluwa <ideraoluwaisa2003@gmail.com>
2024-06-08 11:04:07 -0700

    new commit

--- src/components/about/team.js
+++ src/components/about/team.js
@@ -10,122 +10,122 @@ const team = async ({locale}) => {
           <p className='text-white text-[12px] md:text-[40px] font-medium'>{t('Our Team')}</p>
         <p className='w-[90%] text-center font-normal text-white text-[10px] md:text-[16px] leading-[12.1px] md:leading-[22.3px] mt-[0.5rem] md:mt-[1.2rem]'>{t('Get to know the individuals dedicated to making LifeCom radio an uplifting and entertaining radio station.')}</p>
      </div>
-     <div className='mt-[2.5rem] grid grid-cols-fluid w-[70vw] mx-auto justify-center'>
-     <div className='bg-gradient-to-b from-[#152947] to-[#456CA7] w-[213px] h-[289px] rounded-[4px] pt-[2px] mt-[1.5rem] mx-auto'>
+     <div className='mt-[2.5rem] grid grid-cols-smfluid md:grid-cols-fluid w-full md:w-[70vw] mx-auto justify-center'>
+     <div className='bg-gradient-to-b from-[#152947] to-[#456CA7] w-[119px] h-[168px] md:w-[213px] md:h-[289px] rounded-[4px] pt-[2px] mt-[1.5rem] mx-auto'>
      <div className='w-[calc(100%-4px)] bg-[#0C1821] h-[calc(100%-2px)] mx-auto p-[1rem] flex flex-col justify-center items-center rounded-[4px]'>
       <div>
-      <img src='/images/about/team1.png' className='w-[165px] h-[160px] mx-auto'/>
-     <p className='text-white text-[14px] font-semibold text-left mt-[0.5rem]'>Andrew Lawrence</p>
-     <p className='text-[#CCC9DC] text-[14px] font-semibold'>O.A.P</p> 
+      <img src='/images/about/team1.png' className='w-[93px] h-[94px] md:w-[165px] md:h-[160px] mx-auto'/>
+     <p className='text-white text-[10px] md:text-[14px] font-medium md:font-semibold text-left mt-[0.5rem]'>Happy Akpeyi</p>
+     <p className='text-[#CCC9DC] text-[10px] md:text-[14px] font-medium md:font-semibold'>O.A.P</p> 
      <div className='flex flex-row items-center mt-[0.5rem] gap-[0.5rem]'>
-       <img src='/images/about/Facebook.png' alt='' className='w-[7.8px] h-[14.58px]'/>
-       <img src='/images/about/linkedin.png' alt='' className='w-[14.08px] h-[14.08px]'/>
-       <img src='/images/about/twitter.png' alt='' className='w-[14px] h-[14px]'/>
+       <img src='/images/about/Facebook.png' alt='' className='w-[4.28px] h-[8px] md:w-[7.8px] md:h-[14.58px]'/>
+       <img src='/images/about/linkedin.png' alt='' className='w-[7px] h-[7px] md:w-[14.08px] md:h-[14.08px]'/>
+       <img src='/images/about/twitter.png' alt='' className='w-[7px] h-[7px] md:w-[14px] md:h-[14px]'/>
      </div>
       </div>
       
      </div>
      </div>
-     <div className='bg-gradient-to-b from-[#152947] to-[#456CA7] w-[213px] h-[289px] rounded-[4px] pt-[2px] mt-[1.5rem] mx-auto'>
+     <div className='bg-gradient-to-b from-[#152947] to-[#456CA7] w-[119px] h-[168px] md:w-[213px] md:h-[289px] rounded-[4px] pt-[2px] mt-[1.5rem] mx-auto'>
      <div className='w-[calc(100%-4px)] bg-[#0C1821] h-[calc(100%-2px)] mx-auto p-[1rem] flex flex-col justify-center items-center rounded-[4px]'>
       <div>
-      <img src='/images/about/team1.png' className='w-[165px] h-[160px] mx-auto'/>
-     <p className='text-white text-[14px] font-semibold text-left mt-[0.5rem]'>Andrew Lawrence</p>
-     <p className='text-[#CCC9DC] text-[14px] font-semibold'>O.A.P</p> 
+      <img src='/images/about/team1.png' className='w-[93px] h-[94px] md:w-[165px] md:h-[160px] mx-auto'/>
+     <p className='text-white text-[10px] md:text-[14px] font-medium md:font-semibold text-left mt-[0.5rem]'>Happy Akpeyi</p>
+     <p className='text-[#CCC9DC] text-[10px] md:text-[14px] font-medium md:font-semibold'>O.A.P</p> 
      <div className='flex flex-row items-center mt-[0.5rem] gap-[0.5rem]'>
-       <img src='/images/about/Facebook.png' alt='' className='w-[7.8px] h-[14.58px]'/>
-       <img src='/images/about/linkedin.png' alt='' className='w-[14.08px] h-[14.08px]'/>
-       <img src='/images/about/twitter.png' alt='' className='w-[14px] h-[14px]'/>
+       <img src='/images/about/Facebook.png' alt='' className='w-[4.28px] h-[8px] md:w-[7.8px] md:h-[14.58px]'/>
+       <img src='/images/about/linkedin.png' alt='' className='w-[7px] h-[7px] md:w-[14.08px] md:h-[14.08px]'/>
+       <img src='/images/about/twitter.png' alt='' className='w-[7px] h-[7px] md:w-[14px] md:h-[14px]'/>
      </div>
       </div>
       
      </div>
      </div>
-     <div className='bg-gradient-to-b from-[#152947] to-[#456CA7] w-[213px] h-[289px] rounded-[4px] pt-[2px] mt-[1.5rem] mx-auto'>
+     <div className='bg-gradient-to-b from-[#152947] to-[#456CA7] w-[119px] h-[168px] md:w-[213px] md:h-[289px] rounded-[4px] pt-[2px] mt-[1.5rem] mx-auto'>
      <div className='w-[calc(100%-4px)] bg-[#0C1821] h-[calc(100%-2px)] mx-auto p-[1rem] flex flex-col justify-center items-center rounded-[4px]'>
       <div>
-      <img src='/images/about/team1.png' className='w-[165px] h-[160px] mx-auto'/>
-     <p className='text-white text-[14px] font-semibold text-left mt-[0.5rem]'>Andrew Lawrence</p>
-     <p className='text-[#CCC9DC] text-[14px] font-semibold'>O.A.P</p> 
+      <img src='/images/about/team1.png' className='w-[93px] h-[94px] md:w-[165px] md:h-[160px] mx-auto'/>
+     <p className='text-white text-[10px] md:text-[14px] font-medium md:font-semibold text-left mt-[0.5rem]'>Happy Akpeyi</p>
+     <p className='text-[#CCC9DC] text-[10px] md:text-[14px] font-medium md:font-semibold'>O.A.P</p> 
      <div className='flex flex-row items-center mt-[0.5rem] gap-[0.5rem]'>
-       <img src='/images/about/Facebook.png' alt='' className='w-[7.8px] h-[14.58px]'/>
-       <img src='/images/about/linkedin.png' alt='' className='w-[14.08px] h-[14.08px]'/>
-       <img src='/images/about/twitter.png' alt='' className='w-[14px] h-[14px]'/>
+       <img src='/images/about/Facebook.png' alt='' className='w-[4.28px] h-[8px] md:w-[7.8px] md:h-[14.58px]'/>
+       <img src='/images/about/linkedin.png' alt='' className='w-[7px] h-[7px] md:w-[14.08px] md:h-[14.08px]'/>
+       <img src='/images/about/twitter.png' alt='' className='w-[7px] h-[7px] md:w-[14px] md:h-[14px]'/>
      </div>
       </div>
       
      </div>
      </div>
-     <div className='bg-gradient-to-b from-[#152947] to-[#456CA7] w-[213px] h-[289px] rounded-[4px] pt-[2px] mt-[1.5rem] mx-auto'>
+     <div className='bg-gradient-to-b from-[#152947] to-[#456CA7] w-[119px] h-[168px] md:w-[213px] md:h-[289px] rounded-[4px] pt-[2px] mt-[1.5rem] mx-auto'>
      <div className='w-[calc(100%-4px)] bg-[#0C1821] h-[calc(100%-2px)] mx-auto p-[1rem] flex flex-col justify-center items-center rounded-[4px]'>
       <div>
-      <img src='/images/about/team1.png' className='w-[165px] h-[160px] mx-auto'/>
-     <p className='text-white text-[14px] font-semibold text-left mt-[0.5rem]'>Andrew Lawrence</p>
-     <p className='text-[#CCC9DC] text-[14px] font-semibold'>O.A.P</p> 
+      <img src='/images/about/team1.png' className='w-[93px] h-[94px] md:w-[165px] md:h-[160px] mx-auto'/>
+     <p className='text-white text-[10px] md:text-[14px] font-medium md:font-semibold text-left mt-[0.5rem]'>Happy Akpeyi</p>
+     <p className='text-[#CCC9DC] text-[10px] md:text-[14px] font-medium md:font-semibold'>O.A.P</p> 
      <div className='flex flex-row items-center mt-[0.5rem] gap-[0.5rem]'>
-       <img src='/images/about/Facebook.png' alt='' className='w-[7.8px] h-[14.58px]'/>
-       <img src='/images/about/linkedin.png' alt='' className='w-[14.08px] h-[14.08px]'/>
-       <img src='/images/about/twitter.png' alt='' className='w-[14px] h-[14px]'/>
+       <img src='/images/about/Facebook.png' alt='' className='w-[4.28px] h-[8px] md:w-[7.8px] md:h-[14.58px]'/>
+       <img src='/images/about/linkedin.png' alt='' className='w-[7px] h-[7px] md:w-[14.08px] md:h-[14.08px]'/>
+       <img src='/images/about/twitter.png' alt='' className='w-[7px] h-[7px] md:w-[14px] md:h-[14px]'/>
      </div>
       </div>
       
      </div>
      </div>
-     <div className='bg-gradient-to-b from-[#36AE99] to-[#18718D70] w-[213px] h-[289px] rounded-[4px] pt-[2px] mt-[1.5rem] mx-auto'>
+     <div className='bg-gradient-to-b  from-[#36AE99] to-[#18718D70] w-[119px] h-[168px] md:w-[213px] md:h-[289px] rounded-[4px] pt-[2px] mt-[1.5rem] mx-auto'>
      <div className='w-[calc(100%-4px)] bg-[#0C1821] h-[calc(100%-2px)] mx-auto p-[1rem] flex flex-col justify-center items-center rounded-[4px]'>
       <div>
-      <img src='/images/about/team1.png' className='w-[165px] h-[160px] mx-auto'/>
-     <p className='text-white text-[14px] font-semibold text-left mt-[0.5rem]'>Andrew Lawrence</p>
-     <p className='text-[#CCC9DC] text-[14px] font-semibold'>O.A.P</p> 
+      <img src='/images/about/team1.png' className='w-[93px] h-[94px] md:w-[165px] md:h-[160px] mx-auto'/>
+     <p className='text-white text-[10px] md:text-[14px] font-medium md:font-semibold text-left mt-[0.5rem]'>Happy Akpeyi</p>
+     <p className='text-[#CCC9DC] text-[10px] md:text-[14px] font-medium md:font-semibold'>O.A.P</p> 
      <div className='flex flex-row items-center mt-[0.5rem] gap-[0.5rem]'>
-       <img src='/images/about/Facebook.png' alt='' className='w-[7.8px] h-[14.58px]'/>
-       <img src='/images/about/linkedin.png' alt='' className='w-[14.08px] h-[14.08px]'/>
-       <img src='/images/about/twitter.png' alt='' className='w-[14px] h-[14px]'/>
+       <img src='/images/about/Facebook.png' alt='' className='w-[4.28px] h-[8px] md:w-[7.8px] md:h-[14.58px]'/>
+       <img src='/images/about/linkedin.png' alt='' className='w-[7px] h-[7px] md:w-[14.08px] md:h-[14.08px]'/>
+       <img src='/images/about/twitter.png' alt='' className='w-[7px] h-[7px] md:w-[14px] md:h-[14px]'/>
      </div>
       </div>
       
      </div>
      </div>
-     <div className='bg-gradient-to-b from-[#36AE99] to-[#18718D70] w-[213px] h-[289px] rounded-[4px] pt-[2px] mt-[1.5rem] mx-auto'>
+     <div className='bg-gradient-to-b  from-[#36AE99] to-[#18718D70] w-[119px] h-[168px] md:w-[213px] md:h-[289px] rounded-[4px] pt-[2px] mt-[1.5rem] mx-auto'>
      <div className='w-[calc(100%-4px)] bg-[#0C1821] h-[calc(100%-2px)] mx-auto p-[1rem] flex flex-col justify-center items-center rounded-[4px]'>
       <div>
-      <img src='/images/about/team1.png' className='w-[165px] h-[160px] mx-auto'/>
-     <p className='text-white text-[14px] font-semibold text-left mt-[0.5rem]'>Andrew Lawrence</p>
-     <p className='text-[#CCC9DC] text-[14px] font-semibold'>O.A.P</p> 
+      <img src='/images/about/team1.png' className='w-[93px] h-[94px] md:w-[165px] md:h-[160px] mx-auto'/>
+     <p className='text-white text-[10px] md:text-[14px] font-medium md:font-semibold text-left mt-[0.5rem]'>Happy Akpeyi</p>
+     <p className='text-[#CCC9DC] text-[10px] md:text-[14px] font-medium md:font-semibold'>O.A.P</p> 
      <div className='flex flex-row items-center mt-[0.5rem] gap-[0.5rem]'>
-       <img src='/images/about/Facebook.png' alt='' className='w-[7.8px] h-[14.58px]'/>
-       <img src='/images/about/linkedin.png' alt='' className='w-[14.08px] h-[14.08px]'/>
-       <img src='/images/about/twitter.png' alt='' className='w-[14px] h-[14px]'/>
+       <img src='/images/about/Facebook.png' alt='' className='w-[4.28px] h-[8px] md:w-[7.8px] md:h-[14.58px]'/>
+       <img src='/images/about/linkedin.png' alt='' className='w-[7px] h-[7px] md:w-[14.08px] md:h-[14.08px]'/>
+       <img src='/images/about/twitter.png' alt='' className='w-[7px] h-[7px] md:w-[14px] md:h-[14px]'/>
      </div>
       </div>
       
      </div>
      </div>
-     <div className='bg-gradient-to-b from-[#36AE99] to-[#18718D70] w-[213px] h-[289px] rounded-[4px] pt-[2px] mt-[1.5rem] mx-auto'>
+     <div className='bg-gradient-to-b  from-[#36AE99] to-[#18718D70] w-[119px] h-[168px] md:w-[213px] md:h-[289px] rounded-[4px] pt-[2px] mt-[1.5rem] mx-auto'>
      <div className='w-[calc(100%-4px)] bg-[#0C1821] h-[calc(100%-2px)] mx-auto p-[1rem] flex flex-col justify-center items-center rounded-[4px]'>
       <div>
-      <img src='/images/about/team1.png' className='w-[165px] h-[160px] mx-auto'/>
-     <p className='text-white text-[14px] font-semibold text-left mt-[0.5rem]'>Andrew Lawrence</p>
-     <p className='text-[#CCC9DC] text-[14px] font-semibold'>O.A.P</p> 
+      <img src='/images/about/team1.png' className='w-[93px] h-[94px] md:w-[165px] md:h-[160px] mx-auto'/>
+     <p className='text-white text-[10px] md:text-[14px] font-medium md:font-semibold text-left mt-[0.5rem]'>Happy Akpeyi</p>
+     <p className='text-[#CCC9DC] text-[10px] md:text-[14px] font-medium md:font-semibold'>O.A.P</p> 
      <div className='flex flex-row items-center mt-[0.5rem] gap-[0.5rem]'>
-       <img src='/images/about/Facebook.png' alt='' className='w-[7.8px] h-[14.58px]'/>
-       <img src='/images/about/linkedin.png' alt='' className='w-[14.08px] h-[14.08px]'/>
-       <img src='/images/about/twitter.png' alt='' className='w-[14px] h-[14px]'/>
+       <img src='/images/about/Facebook.png' alt='' className='w-[4.28px] h-[8px] md:w-[7.8px] md:h-[14.58px]'/>
+       <img src='/images/about/linkedin.png' alt='' className='w-[7px] h-[7px] md:w-[14.08px] md:h-[14.08px]'/>
+       <img src='/images/about/twitter.png' alt='' className='w-[7px] h-[7px] md:w-[14px] md:h-[14px]'/>
      </div>
       </div>
       
      </div>
      </div>
-     <div className='bg-gradient-to-b from-[#36AE99] to-[#18718D70] w-[213px] h-[289px] rounded-[4px] pt-[2px] mt-[1.5rem] mx-auto'>
+     <div className='bg-gradient-to-b  from-[#36AE99] to-[#18718D70] w-[119px] h-[168px] md:w-[213px] md:h-[289px] rounded-[4px] pt-[2px] mt-[1.5rem] mx-auto'>
      <div className='w-[calc(100%-4px)] bg-[#0C1821] h-[calc(100%-2px)] mx-auto p-[1rem] flex flex-col justify-center items-center rounded-[4px]'>
       <div>
-      <img src='/images/about/team1.png' className='w-[165px] h-[160px] mx-auto'/>
-     <p className='text-white text-[14px] font-semibold text-left mt-[0.5rem]'>Andrew Lawrence</p>
-     <p className='text-[#CCC9DC] text-[14px] font-semibold'>O.A.P</p> 
+      <img src='/images/about/team1.png' className='w-[93px] h-[94px] md:w-[165px] md:h-[160px] mx-auto'/>
+     <p className='text-white text-[10px] md:text-[14px] font-medium md:font-semibold text-left mt-[0.5rem]'>Happy Akpeyi</p>
+     <p className='text-[#CCC9DC] text-[10px] md:text-[14px] font-medium md:font-semibold'>O.A.P</p> 
      <div className='flex flex-row items-center mt-[0.5rem] gap-[0.5rem]'>
-       <img src='/images/about/Facebook.png' alt='' className='w-[7.8px] h-[14.58px]'/>
-       <img src='/images/about/linkedin.png' alt='' className='w-[14.08px] h-[14.08px]'/>
-       <img src='/images/about/twitter.png' alt='' className='w-[14px] h-[14px]'/>
+       <img src='/images/about/Facebook.png' alt='' className='w-[4.28px] h-[8px] md:w-[7.8px] md:h-[14.58px]'/>
+       <img src='/images/about/linkedin.png' alt='' className='w-[7px] h-[7px] md:w-[14.08px] md:h-[14.08px]'/>
+       <img src='/images/about/twitter.png' alt='' className='w-[7px] h-[7px] md:w-[14px] md:h-[14px]'/>
      </div>
       </div>
       
